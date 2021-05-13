a9bd8eb13237d6522e0c4b2a845769fb0ad9d965
luleano <luleano@icloud.com>
2021-05-13 15:47:22 -0500

    Deleting things we dont need

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Router from './router/';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
