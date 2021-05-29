bd40c4a4f29d698b822bbc193f696c069a2d5cf5
luleano <luleano@icloud.com>
2021-05-29 01:44:01 -0500

    Quiz component working correctly. It sections the total questions showing by 5. It only allows to answer the current question by blocking the rest. Also scroll to top when user completes one section.

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Radio } from 'antd';
+import { Progress } from 'antd';
+import { animateScroll as scroll } from 'react-scroll';
 
 import HSSADAPI from '../../apis/HSSADApi';
 
 import * as S from './styles';
+import Question from './Question';
+import Button from '../../common/Button';
 
 const Quiz = () => {
     const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [sectionCount, setSectionCount] = useState(0);
+    const [answers, setAnswers] = useState([]);
+    const [count, setCount] = useState(0);
+
 
     useEffect(() => {
         getQuestions();
@@ -20,21 +28,45 @@ const Quiz = () => {
             console.log(err);
         }
     }
+
+    const scrollToTop = () =>{
+        scroll.scrollToTop();
+    }
+
+    const handleSelectedAnswer = ( value ) => {
+        setAnswers( arr => [...arr, value]);
+        setCurrentQuestion(currentQuestion+1);
+        setCount(count+1);
+    
+    };
+
+    const handleNextSection = () =>{
+        scrollToTop();
+        setCount(0);
+        setSectionCount(sectionCount+5);
+    }
+
+    const handleFinish = () => {
+        console.log('Quiz finished');
+    }
+    
+    let sampleQuestions = questions.slice(sectionCount,sectionCount+5);
+    let isCompleted = answers.length===20 ? true : false;
     return (
         <S.QuestionsWrapper>
-            { questions.map((question, index) => (
-                <S.Question key={question.qNumber}>
-                    <S.Statement>
-                        {question.description}
-                    </S.Statement>
-                    <Radio.Group style={{paddingTop: '1.5rem'}}>
-                    <Radio>Nunca</Radio>
-                    <Radio>A veces</Radio>
-                    <Radio>Casi siempre</Radio>
-                    <Radio>Siempre</Radio>
-                </Radio.Group>
-                </S.Question>
-            )) }
+            <Progress percent={ 5 * currentQuestion }/>
+            { sampleQuestions.map((question, index) => (
+                <Question
+                    key= { index }
+                    qNumber = { question.qNumber }
+                    description = { question.description }
+                    handleSelectedAnswer = { handleSelectedAnswer }
+                    isActive = { currentQuestion===question.qNumber-1 ? true: false }
+                    isDisabled = { currentQuestion===question.qNumber-1 ? false: true }
+                />
+            ))}
+            { !isCompleted && count===5 && <Button onClick={ handleNextSection }color={'#0000CC'}>Next</Button> }
+            { isCompleted && count===5 && <Button onClick={ handleFinish }color={'#0000CC'}>Finish</Button> }
         </S.QuestionsWrapper>
     );
 };
--- src/components/Quiz/styles.js
+++ src/components/Quiz/styles.js
@@ -9,21 +9,3 @@ export const QuestionsWrapper = styled.div`
     align-items: center;
     padding: 15px;
 `
-export const Question = styled.div`
-    padding: 35px 0 20px;
-    width: 100%;
-    max-width: 400px;
-    border-bottom: 1px solid #dcdcdc;
-`
-export const Statement = styled.div`
-    color: #576071;
-    font-weight: 600;
-    text-align: center;
-`
-
-export const Option = styled.div`
-    border-radius: 50%
-    cursor: pointer;
-    display: flex;
-    border: 2px solid #56ac8a;
-`
