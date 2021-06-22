9500b7cc0a6c60298472a971a7a657ff656c88d6
luleano <luleano@icloud.com>
2021-06-21 23:28:05 -0500

    Adding Result Modal to the Survey module. Replacing the useState hook by useReducer in order to handle multiple changes to the component's state.

--- src/components/Survey/index.jsx
+++ src/components/Survey/index.jsx
@@ -1,35 +1,51 @@
-import React, {useState } from 'react';
+import React, {useReducer } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Quiz from '../Survey/Quiz';
 import SurveyForm from './SurveyForm';
 import HSSADAPI from '../../apis/HSSADApi';
+import ResultModal from './ResultModal';
 
 const Survey = () => {
-    const [data, setData] = useState();
-    const [showQuiz, setShowQuiz] = useState(false);
+
+    const history = useHistory();
+    //Little hack using useReducer to be able to update 2 or more states at the same time!
+    const [state, setState] = useReducer(
+        (state, newState) => ({...state, ...newState}),
+        {data: null, showQuiz: false, isModalVisible: false, prediction: null}
+    );
+
+    const showModal = () => {
+        setState({isModalVisible: true});
+    };
+
+    const handleOk = () => {
+        history.replace('/home');       
+        setState({isModalVisible: false});
+        
+    };
     
     const sendData = async (data) => {
         try{
             const response = await HSSADAPI.post('/survey', data);
-            console.log("respuesta"+response);
+            setState({prediction: response.data.prediction});
         } catch(err){
             console.log(err);
         }
     }
     const completeSurvey = (data) =>{
-        setData(data);
-        setShowQuiz(true);
+        setState({data: data});
+        setState({showQuiz: true});
     };
 
     const completeQuiz = (answers) => {
-        sendData({...data, answers});
-        setData({...data, answers});
+        sendData({...state.data, answers});
+        showModal();
     };
-
     return (
-        <>  
-            { console.log(data) }
-            { !showQuiz ? <SurveyForm completeSurvey={ completeSurvey }/> : <Quiz completeQuiz={ completeQuiz }/>}
+        <> 
+            { !state.showQuiz ? <SurveyForm completeSurvey={ completeSurvey }/> : <Quiz completeQuiz={ completeQuiz }/>}
+            <ResultModal isModalVisible={state.isModalVisible} prediction={state.prediction} handleOk={handleOk} />
         </>
     );
 };
