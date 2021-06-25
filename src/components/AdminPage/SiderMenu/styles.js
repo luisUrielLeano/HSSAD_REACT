a6f3ae20cc9b7078ea6fd96bfc149732a2c26763
luleano <luleano@icloud.com>
2021-06-25 15:30:05 -0500

    SiderMenu created to handle the links to the new routes into AdminPage

---
+++ src/components/AdminPage/SiderMenu/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const LogoContainer = styled(Link)`
+  display: flex;
+  padding-top: .5rem;
+  padding-left: 3rem;
+`;
